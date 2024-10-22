d7c7bcfd44cfb650951413c146129475772ac702
cechecco <desalvadorfrancesco@gmail.com>
2024-10-22 14:12:25 +0000

    improve feature header

--- veda/app/dashboard/page.tsx
+++ veda/app/dashboard/page.tsx
@@ -4,8 +4,7 @@ import React, { useEffect, useState } from "react"
 import { Feature, getFeature, getFeatures } from "./actions"
 import Breadcrumb from './Breadcrumb';
 import FeatureList from './FeatureList';
-import { Button } from "@/components/ui/button";
-import { Pencil, Plus } from "lucide-react";
+import FeatureHeader from './FeatureHeader';
 
 const DashboardPage: React.FC = () => {
 
@@ -47,33 +46,7 @@ const DashboardPage: React.FC = () => {
         breadcrumb={breadcrumb}
         setParentId={setParentId}
       />
-      <div className="p-2 flex justify-between items-center">
-        <div className="flex items-center gap-2">
-          <p className="text-2xl font-bold">
-            {feature ? feature.name : "My company"}
-          </p>
-          {feature && (
-            <Button
-              variant="outline"
-              size="icon"
-            >
-              <Pencil className="h-4 w-4" />
-            </Button>
-          )}
-        </div>
-        <Button
-          variant="outline"
-          size="icon"
-        >
-          <Plus className="h-4 w-4" />
-        </Button>
-      </div>
-      
-      <p className="text-muted-foreground p-2 text-justify">
-        {feature
-          ? feature.description
-          : "This is your company dashboard. Here you can manage your company projects"}
-      </p>
+      <FeatureHeader feature={feature} />
       <FeatureList
         features={subFeatures}
         setParentId={setParentId}
